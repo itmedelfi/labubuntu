303bacf8b92d61071a47a5c079fb20caefadf16f
ktikg <catherineg0502@gmail.com>
2026-02-14 02:21:15 -0300

    Update TP1_Análisis_de_Datos.py
    cambie ciertos nombre para que coincidieran con como estan en las tablas

--- TP1_Análisis_de_Datos.py
+++ TP1_Análisis_de_Datos.py
@@ -16,9 +16,9 @@ carpeta = 'ubicacion...'
 
 provincias                       = pd.read_csv(carpeta + "/provincias.csv")
 departamentos                    = pd.read_csv(carpeta + "/departamentos.csv")
-habitantes                      = pd.read_csv(carpeta + "/habitantes.csv")
-defunciones                     = pd.read_csv(carpeta + "/defunciones.csv")
-establecimientos_medicos        = pd.read_csv(carpeta + "/establecimientos_medicos.csv")
+habitantes                       = pd.read_csv(carpeta + "/habitantes.csv")
+defunciones                      = pd.read_csv(carpeta + "/defunciones.csv")
+establecimientos_medicos         = pd.read_csv(carpeta + "/establecimientos_medicos.csv")
 
 #%%
 # TABLAS ÚTILES
@@ -28,7 +28,7 @@ unir_provincias_habitantes = """
                 SELECT p.nombre AS nombre_prov, h.sexo, h.año_censo, h.cantidad, h.grupo_etario, h.cobertura
                 FROM provincias AS p
                 INNER JOIN habitantes AS h
-                ON p.id == h.id_provincia
+                ON p.id == h.provincia_id
               """
 
 provincia_x_habitantes = dd.query(unir_provincias_habitantes).df()
@@ -40,15 +40,15 @@ consulta_i = """
     SELECT nombre_prov AS Provincia, grupo_etario, 
     
     -- Columnas para el año 2010
-    SUM(CASE WHEN año = 2010 AND cobertura = 'privada' THEN cantidad ELSE 0 END) 
+    SUM(CASE WHEN año = 2010 AND cobertura = 'Privada' THEN cantidad ELSE 0 END) 
         AS Habitantes_con_cobertura_en_2010,
-    SUM(CASE WHEN año = 2010 AND cobertura = 'publica' THEN cantidad ELSE 0 END) 
+    SUM(CASE WHEN año = 2010 AND cobertura = 'Pública' THEN cantidad ELSE 0 END) 
         AS Habitantes_sin_cobertura_en_2010,
     
     -- Columnas para el año 2022
-    SUM(CASE WHEN año = 2022 AND cobertura = 'privada' THEN cantidad ELSE 0 END) 
+    SUM(CASE WHEN año = 2022 AND cobertura = 'Privada' THEN cantidad ELSE 0 END) 
         AS Habitante_con_cobertura_en_2022,
-    SUM(CASE WHEN año = 2022 AND cobertura = 'publica' THEN cantidad ELSE 0 END) 
+    SUM(CASE WHEN año = 2022 AND cobertura = 'Pública' THEN cantidad ELSE 0 END) 
         AS Habitantes_sin_cobertura_en_2022
 FROM Habitantes
 GROUP BY Provincia, grupo_etario
@@ -60,14 +60,14 @@ reporte_i = dd.query(consulta_i).df()
 #%%
 # ii)
 consulta_ii = """
-    SELECT d.Provincia, e.financiacion,
+    SELECT d.provincia_id, e.financiamiento,
     SUM(CASE WHEN e.terapia_intensiva = 'si' THEN 1 ELSE 0 END) AS cantidad_establecimientos
-    FROM Establecimientos_medicos AS e
+    FROM establecimientos_medicos AS e
     
-    JOIN Departamentos_por_provincia AS d 
-        ON e.departamento = d.departamentos_asociados
-    GROUP BY d.Provincia, e.financiacion
-    ORDER BY d.Provincia, e.financiacion
+    JOIN departamentos AS d 
+        ON e.departamento_id = d.departamento_id
+    GROUP BY d.provincia_id, e.financiamiento
+    ORDER BY d.provincia_id, e.financiamiento
 """
 
 reporte_ii = dd.query (consulta_ii).df()
@@ -86,17 +86,16 @@ reporte_iii = dd.query (consulta_iii).df()
 # iv)
 consulta_iv = """
     SELECT 
-        h.Provincia, 
+        h.nombre, 
         h.grupo_etario,
         (SUM(d.cantidad) * 1000.0 / SUM(h.cantidad)) AS tasa_mortalidad_1000
-    FROM provincia_x_habitantes AS h
-    JOIN Defunciones d ON h.Provincia = d.Provincia 
+    FROM provincias_x_habitantes AS h
+    JOIN defunciones d ON h.provincia_id = d.provincia_id 
         AND h.grupo_etario = d.grupo_etario
-        AND h.Año = d.Año
-    WHERE h.Año = 2022
-    GROUP BY h.Provincia, h.grupo_etario
-    ORDER BY h.Provincia, h.grupo_etario
-
+        AND h.año = d.año
+    WHERE h.año = 2022
+    GROUP BY h.nombre, h.grupo_etario
+    ORDER BY h.nombre, h.grupo_etario
  """
 
 reporte_iv = dd.query (consulta_iv).df()
@@ -105,15 +104,16 @@ reporte_iv = dd.query (consulta_iv).df()
 # v)
 consulta_v = """
     SELECT 
-        Causa,
-        SUM(CASE WHEN Año = 2022 THEN cantidad ELSE 0 END) - 
-        SUM(CASE WHEN Año = 2010 THEN cantidad ELSE 0 END) AS diferencia_defunciones
-    FROM Defunciones
-    GROUP BY Causa
+        descripcion,
+        SUM(CASE WHEN año = 2022 THEN cantidad ELSE 0 END) - 
+        SUM(CASE WHEN año = 2010 THEN cantidad ELSE 0 END) AS diferencia_defunciones
+    FROM defunciones
+    GROUP BY descripcion
     ORDER BY diferencia_defunciones DESC
  """
 
 reporte_v = dd.query (consulta_v).df()
+
 
 
 
